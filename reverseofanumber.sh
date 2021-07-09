c729582ffca4bab8caac41984c638b268f7c68b3
Priybhash Soni    प्रियभाष सोनी <py.priybhashsoni@gmail.com>
2021-07-09 20:24:51 +1100

    Create reverseofanumber.sh

---
+++ reverseofanumber.sh
@@ -0,0 +1,11 @@
+read -p "Enter the any number >> " n
+rev=0
+t=$n
+while [ $t -gt 0 ]
+do
+    rem=$(( $t % 10 ))
+    rev=$(( $rev * 10 + $rem ))
+    t=$(( $t / 10 ))
+done
+
+echo "Reverse Number is $rev"
